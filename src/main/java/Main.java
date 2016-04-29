b38644f195751e07d1120310734b9fd15aa585df
Peter-Paul <PP.vanderMark@student.han.nl>
2016-04-29 11:04:50 +0200

    main werkt

--- src/main/java/Main.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Peter-Paul on 26/04/2016.
- */
-public class Main {
-
-}
-
---
+++ src/main/java/main/Main.java
@@ -0,0 +1,29 @@
+package main;
+
+/**
+ * Created by Peter-Paul on 29/04/2016.
+ */
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
+
