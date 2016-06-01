7faa73837aa30f2d04cbf7e78078e3a7ecf72718
Peter-Paul <PP.vanderMark@student.han.nl>
2016-06-01 15:42:46 +0200

    fnwvjo

--- src/main/java/datasource/DAO/WedstrijdenDao.java
+++ src/main/java/datasource/DAO/WedstrijdenDao.java
@@ -1,5 +1,6 @@
 package datasource.DAO;
 
+import domain.SpelerInWedstrijd;
 import domain.Tafel;
 import domain.Wedstrijd;
 
@@ -32,26 +33,52 @@ public class WedstrijdenDao extends DAO {
         return tafels;
     }
 
-//    public ArrayList<Wedstrijd> getwedstrijden(int toernooiId){
-//        ArrayList<Tafel> tafels = new ArrayList<>();
-//        try{
-//            connect();
-//            PreparedStatement preparedStatement = conn.prepareStatement("SELECT Row_Number() Over(ORDER BY TAFELNR),toernooinr, TAFELNR, Wedstrijdnr FROM tafels WHERE toernooinr = ?");
-//            preparedStatement.setInt(1, toernooiId);
-//            ResultSet rs = preparedStatement.executeQuery();
-//
-//
-//            while(rs.next()){
-//                tafels.add(new Tafel("Tafel "+rs.getInt(1), rs.getInt(2), rs.getInt(3), rs.getInt(4)));
-//            }
-//
-//            disconnect();
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        return tafels;
-//    }
+    public ArrayList<Wedstrijd> getwedstrijden(int toernooiId){
+        ArrayList<Wedstrijd> wedstrijd = new ArrayList<>();
+        try{
+            connect();
+            PreparedStatement preparedStatement = conn.prepareStatement("select d.DeelToernooinr, w.TeWinnenRondes, w.Wedstrijdnr, w.DeelToernooinr, d.Startdatum, t.TAFELNR, di.Team, dn.Voornaam, dn.Achternaam, dn.Bondsnr from DeelToernooi d \n" +
+                                                                        "inner join \n" +
+                                                                        "Wedstrijd w on  d.DeelToernooinr = w.DeelToernooinr \n" +
+                                                                        "inner join \n" +
+                                                                        "DeelnemerInWedstrijd di on di.Wedstrijdnr = w.Wedstrijdnr\n" +
+                                                                        "inner join \n" +
+                                                                        "Deelnemer dn on dn.Deelnemernr = di.Deelnemernr\n" +
+                                                                        "left join Tafels t on t.Wedstrijdnr = w.Wedstrijdnr  \n" +
+                                                                        "where d.Toernooinr = ?");
+            preparedStatement.setInt(1, toernooiId);
+            ResultSet rs = preparedStatement.executeQuery();
+            PreparedStatement preparedStatement2 = conn.prepareStatement("select d.DeelToernooinr, w.TeWinnenRondes, w.Wedstrijdnr, w.DeelToernooinr, d.Startdatum, t.TAFELNR from DeelToernooi d \n" +
+                                                                         "left join \n" +
+                                                                         "Wedstrijd w on  d.DeelToernooinr = w.DeelToernooinr \n" +
+                                                                         "left join Tafels t on t.Wedstrijdnr = w.Wedstrijdnr  \n" +
+                                                                         "where d.Toernooinr = ?");
+
+            preparedStatement2.setInt(1, toernooiId);
+            ResultSet rs2 = preparedStatement.executeQuery();
+
+            while(rs2.next()){
+                wedstrijd.add(new Wedstrijd(rs.getInt(1), rs.getInt(2),rs.getInt(3),rs.getTimestamp(4).toLocalDateTime(),rs.getInt(5)));
+            }
+
+            while(rs.next()){
+                for(Wedstrijd w : wedstrijd){
+                    if(rs.getInt(3) == w.getWedstrijdnr()){
+                        if(rs.getBoolean(7)){
+                            w.addSpeler2(new SpelerInWedstrijd(rs.getString(8),rs.getString(9), rs.getInt(10)));
+                        }else{
+                            w.addSpeler1(new SpelerInWedstrijd(rs.getString(8),rs.getString(9), rs.getInt(10)));
+                        }
+                    }
+                }
+            }
+            disconnect();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return wedstrijd;
+    }
 
     public void addTafels(int toernooinummer, int aantal){
         try {
