5777a569c622ccec4c3416a4fde98e6efc5b5115
Peter-Paul <PP.vanderMark@student.han.nl>
2016-06-02 14:01:26 +0200

    fnwvjo

--- src/main/java/datasource/DAO/WedstrijdenDao.java
+++ src/main/java/datasource/DAO/WedstrijdenDao.java
@@ -49,14 +49,14 @@ public class WedstrijdenDao extends DAO {
                                                                         "inner join \n" +
                                                                         "Deelnemer dn on dn.Deelnemernr = di.Deelnemernr\n" +
                                                                         "left join Tafels t on t.Wedstrijdnr = w.Wedstrijdnr  \n" +
-                                                                        "where d.Toernooinr = ?");
+                                                                        "where d.Toernooinr = ? and w.TeWinnenRondes >= 1");
             preparedStatement.setInt(1, toernooiId);
             ResultSet rs = preparedStatement.executeQuery();
             PreparedStatement preparedStatement2 = conn.prepareStatement("select d.DeelToernooinr, w.TeWinnenRondes, w.Wedstrijdnr, d.Startdatum, t.TAFELNR from DeelToernooi d \n" +
                                                                          "left join \n" +
                                                                          "Wedstrijd w on  d.DeelToernooinr = w.DeelToernooinr \n" +
                                                                          "left join Tafels t on t.Wedstrijdnr = w.Wedstrijdnr  \n" +
-                                                                         "where d.Toernooinr = ?");
+                                                                         "where d.Toernooinr = ? and w.TeWinnenRondes >= 1 and exists (select 1 from DeelnemerInWedstrijd di where di.Wedstrijdnr = w.Wedstrijdnr)");
 
             preparedStatement2.setInt(1, toernooiId);
             ResultSet rs2 = preparedStatement2.executeQuery();
